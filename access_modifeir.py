6449a9c1e1ef6af875611d48a7d54cb8d2af7e5f
singhshejal30-cloud <singhshejal30@gmail.com>
2026-02-02 09:08:42 +0530

    Add files via upload

---
+++ access_modifeir.py
@@ -0,0 +1,73 @@
+#public
+# class aipa:
+#     def __init__(self,name):
+#         self.name=name
+#     def display(self):
+#         print(self.name)
+# s= aipa("komal")
+# s.display()
+# print(s.name)
+
+# #private
+# class AIPA:
+#     def __init__(self,name,tools):
+#         self.name=name
+#         self._tools=tools
+#     def uses(self):
+#         print(self._tools)
+# t=AIPA("shejal","almirah")
+# t.uses()
+# print(t._tools)
+
+# #protected
+# class AIPA:
+#     def __init__(self,name,tools,register):
+#         self.name=name
+#         self._tools=tools
+#         self.__register=register 
+#     def classregister(self):  
+#         print(self.__register)
+# r=AIPA("shejal","laptop","CR")
+# print(r.name,r._tools,r._AIPA__register)
+
+#PRACTICE
+#creat a class studentrecord
+#public
+
+# class aipa:
+#     def __init__(self,student_name):
+#         self.student_name=student_name
+#     def display(self):
+#         print(self.student_name)
+# s = aipa("shejal")
+# s.display()
+# print(s.student_name)
+
+#private variable:
+# class aipa:
+#     def __init__(self,student_name,roll_no):
+#         self.student_name=student_name
+#         self._roll_no=roll_no
+#     def uses(self):
+#         print(self._roll_no)
+# t = aipa("shejal","1")
+# t.uses()
+# print(t._roll_no)
+
+# #protected
+# class aipa:
+#     def __init__(self,student_name,roll_no,marks):
+#         self.student_name=student_name
+#         self._roll_no=roll_no
+#         self.__marks=marks
+#     def classname(self):
+#         print(self.__marks)
+# z= aipa("shejal","1","90%")
+# print(z.student_name,z._roll_no,z._aipa__marks)
+#             
+
+
+
+
+    
+
