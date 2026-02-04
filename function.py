7dd606dec74574cf72d7e6b6076d35c746b629b0
Shejal Singh <singhshejal30@gmail.com>
2026-02-04 09:08:44 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,39 @@
+def add(a,b):
+    return a+b
+print(add(10,10))
+print(add(20,30))
+
+def sub(x,y):
+    z = x-y
+    return z
+print(sub(10,7))
+
+def aipa():
+    print("this is a trade in NSTI")
+aipa()
+
+
+def AIPA(address,state):
+    print("this is a trade in NSTI",address,"in",state)
+AIPA("prayagraj","uttarpradesh") 
+
+def div(a,b):
+    if b!=0:
+        return a/b
+    else:
+        return"0"
+print(div(10,0))
+
+
+def square(a):
+    return a**2
+print(square(10))
+
+def multi(a):
+    return a*2
+print(multi(10))
+
+def square(a):
+    return a**2
+
+    
