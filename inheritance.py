557f34cf567df0da29e6175cc6efec9fb8746757
Shejal Singh <singhshejal30@gmail.com>
2026-02-06 12:23:54 +0530

    Add files via upload

---
+++ inheritance.py
@@ -0,0 +1,84 @@
+# class grandfather:
+#     def house(self):
+#         print("this is grandfather house")
+
+# class father(grandfather):
+#     def car(self):
+#         print("this is father's car")
+
+# class son(father):
+#     def bike(self):
+#         print("this is son's bike")
+
+# g=grandfather()
+# g.house()
+# f=father()
+# f.car()
+# f.house()
+# s=son()
+# s.house()
+# s.car()
+# s.bike()
+
+# class NSTI:
+#     def campus(self):
+#         print("this is NSTI campus")
+# class Trade:
+#     def teacher(self):
+#         print("there are 40 staff")
+# class std:
+#     def students(self):
+#         print("there are 500 students")
+
+# n=NSTI()
+# t=Trade() 
+# s=std()
+# n.campus()
+# t.teacher()
+# s.students()
+
+
+# #right
+# class NSTI:
+#     def campus(self):
+#         print("this is NSTI campus")
+# class Trade(NSTI):
+#     def teacher(self):
+#         print("there are 40 staff")
+# class std(Trade):
+#     def students(self):
+#         print("there are 500 students")
+# n=NSTI()
+# print()
+# n.campus()
+
+# t=Trade()
+# print()
+# t.campus()
+# print()
+# t.teacher()
+
+# s=std()
+# print()
+# s.campus()
+# print()
+# s.teacher()
+# print()
+# s.students()
+        
+
+
+
+# class a:
+#     def name():
+#       print("shejal")
+# class b:
+#     def ser_name():
+#         print("singh")
+# class c(a,b):
+#     def qualifications():
+#         print("this is a multi talented girl")
+# object = (c)
+# object.name()
+# object.ser_name()
+# object.qualifications()
