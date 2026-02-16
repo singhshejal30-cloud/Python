7d4b1ffb306fb1ea049a8f9b388dc53a989faf91
Shejal Singh <singhshejal30@gmail.com>
2026-02-16 09:35:35 +0530

    Add files via upload

---
+++ print.py
@@ -0,0 +1,39 @@
+# print("hello world")
+# print("shejal")
+# print("shejal\nsingh")
+# print(""" os is the bridge that connects between hardware and software""")
+
+# print(f"the result of {15} + {4} is: {15 + 4 }")
+# print(f"the result of {15} - {4} is: {15 - 4 }")
+# print(f"the result of {15} * {4} is: {15 * 4 }")
+# print(f"the result of {15} / {4} is: {15 / 4 }")
+
+# #inserting space between outputs using commas
+# print("the sum of 5 and 3 is", 5 + 3)
+# print("the product of 4 and 7 is", 4 * 7)
+
+# #custom separator between outputs
+# print("5", "+", "3", "=", sep=" ")
+# print("4", "*", "7", "=", 4 * 7, sep="*")
+
+#inserting text at the end of a print statement using end parameter
+# print("5 + 3= ", end="")
+# print(5 + 3)
+# print("4 * 7= ", end=" ")
+# print(4 * 7)
+
+# #this is single-line comment
+# x = 10
+# print(x)
+
+# #multi-line printing with mulyiple print statements
+# print("hello, world")
+# print("welcome to python programming.")
+# print("this is multi-line printing.")
+
+
+
+# import function
+
+# print(function.square(10))
+
